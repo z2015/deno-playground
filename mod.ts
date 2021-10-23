7d3b39e9bcf5f95333fa3fb3f7ac7e872b171634
Jan William <janehochou@gmail.com>
2021-10-23 12:42:32 +0800

    fix import

--- mod.ts
+++ mod.ts
@@ -1,5 +1,5 @@
 import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
-import {getStocks} from './controllers/stocks';
+import {getStocks} from './controllers/stocks.ts';
 
 async function handleRequest(request: Request): Promise<Response> {
   const { pathname } = new URL(request.url);
