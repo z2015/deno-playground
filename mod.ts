cb19b13fb933a3c2f937c5d173c5723b55a186b0
Jan William <janehochou@gmail.com>
2021-10-23 13:17:50 +0800

    server static

--- mod.ts
+++ mod.ts
@@ -1,26 +1,11 @@
 import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 import {getStocks} from './controllers/stocks.ts';
 
+const root = './public';
+
 async function handleRequest(request: Request): Promise<Response> {
   const { pathname } = new URL(request.url);
   console.log(pathname);
-
-  // This is how the server works:
-  // 1. A request comes in for a specific asset.
-  // 2. We read the asset from the file system.
-  // 3. We send the asset back to the client.
-
-  // Check if the request is for style.css.
-  if (pathname.startsWith("/style.css")) {
-    // Read the style.css file from the file system.
-    const file = await Deno.readFile("./style.css");
-    // Respond to the request with the style.css file.
-    return new Response(file, {
-      headers: {
-        "content-type": "text/css",
-      },
-    });
-  }
 
   if (pathname === '/code.json') {
     const data = await getStocks();
@@ -42,6 +27,13 @@ async function handleRequest(request: Request): Promise<Response> {
       }
     )
   }
+
+    let file = root + pathname;
+    if (pathname === '/') {
+      file = root + 'index.html';
+    }
+    const fileBlob = await Deno.readFile(file);
+    return new Response(fileBlob);
   
 }
 
