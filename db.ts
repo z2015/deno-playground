144cf7d3124e5e724787203557410ae882e90878
Jan William <janehochou@gmail.com>
2021-10-23 10:20:29 +0800

    console uri

--- db.ts
+++ db.ts
@@ -4,9 +4,8 @@ const user = Deno.env.MONGO_USER;
 const pwd = Deno.env.MONGO_PWD;
 const db = Deno.env.MONGO_DB;
 
-console.log(Deno.env);
-
 const uri = `mongodb+srv://${user}:${pwd}@cluster0.sls1x.mongodb.net/${db}?authMechanism=SCRAM-SHA-1&retryWrites=true&w=majority`;
+console.log(uri);
 
 const client = new MongoClient()
 await client.connect(uri)
